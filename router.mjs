e9a765e4ed6c74e074239d86d6d6976449bae209
PeterAlexandru <lxndr_peter@yahoo.com>
2023-03-07 15:19:11 +0000

    added files

--- router.mjs
+++ router.mjs
@@ -5,5 +5,6 @@ const router = express.Router() ;
 // Routes
 router.get("/", events.index) ; // Get all events
 router.delete("/delete", events.remove) ; // Remove event by id
-
+router.post("/add" , events.add) ; // Add new event
+router.put("/put", events.update) ; // Update event by id
 export default router ;
