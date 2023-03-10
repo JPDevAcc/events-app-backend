778958e59f62a88b87dabfe9eb1854cf2f0e0a16
JP <71121804+JPDevAcc@users.noreply.github.com>
2023-03-10 20:25:41 +0000

    Additional validation

--- eventsController.mjs
+++ eventsController.mjs
@@ -23,16 +23,33 @@ export async function remove(req, res) {
 	}
 }
 
-// TODO: Add validation / error handling
+// Add event
 export async function add(req, res) {
-    const newEvent = req.body;
-    const event = new Event(newEvent);
-    await event.save();
-    res.send({ message: "New event inserted." });
+	if (!ensurePresent(req.body, ['title', 'location', 'date', 'duration', 'durationUnits', 'description', 'picture'])) {
+		res.status(400).send({message: "Bad request"})
+		return ;
+	}
+	const newEvent = req.body;
+	const event = new Event(newEvent);
+	await event.save();
+	res.send({ message: "New event inserted" });
 }
 
-// TODO: Add validation / error handling
-export async function update(req, res){
-    await Event.findOneAndUpdate({ _id:req.params.id }, req.body);
-    res.send({ message: "Event updated." });
-  };
+// Update an event (all fields)
+export async function update(req, res) {
+	if (!ensurePresent(req.body, ['title', 'location', 'date', 'duration', 'durationUnits', 'description', 'picture'])) {
+		res.status(400).send({message: "Bad request"})
+		return ;
+	}
+
+  const event = await Event.findOneAndUpdate({ _id:req.params.id }, req.body);
+	if (event) res.send({ message: "Event updated" }) ;
+	else res.status(404).send({message: "Event not found"})
+};
+
+function ensurePresent(data, fields) {
+	for (const field of fields) {
+		if (data[field] === undefined || data[field] === null) return false ;
+	}
+	return true ;
+}
